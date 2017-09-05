94c838b30460992a0043d08b7cf7163524bffd40
Todd Atkins <toddatkins01@gmail.com>
2017-09-05 17:16:24 -0400

    AgeInFiveYears final

--- src/codingbydoingproblems/AgeInFiveYears.java
+++ src/codingbydoingproblems/AgeInFiveYears.java
@@ -6,10 +6,10 @@ public class AgeInFiveYears {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
-		System.out.print("Hello. What is your name?");
+		System.out.print("Hello. What is your name? ");
 		String name = input.nextLine();
 
-		System.out.print("\nHi, " + name + "! How old are you?");
+		System.out.print("\nHi, " + name + "! How old are you? ");
 		int age = input.nextInt();
 
 		System.out.print("\nDid you know that in five years you will be " + (age + 5) + " years old?");
